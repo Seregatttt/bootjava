abb0fdd6f3cb8cce6c9d2eea67d7186f2fb53b24
seregatttt <seregatttt@mail.ru>
2020-06-26 11:14:14 +0300

    2 04 role auth

--- src/main/java/ru/javaops/bootjava/restaurantvoting/model/Role.java
+++ src/main/java/ru/javaops/bootjava/restaurantvoting/model/Role.java
@@ -2,12 +2,13 @@ package ru.javaops.bootjava.restaurantvoting.model;
 
 import org.springframework.security.core.GrantedAuthority;
 
+//https://stackoverflow.com/a/19542316/548473
 public enum Role implements GrantedAuthority {
-    ROLE_USER,
-    ROLE_ADMIN;
+    USER,
+    ADMIN;
 
     @Override
     public String getAuthority() {
-        return name();
+        return "ROLE_" + name();
     }
 }
